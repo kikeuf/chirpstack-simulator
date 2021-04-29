0e2e482393b265887884e260dc9ff412d06f5a16
kikeuf <82982743+kikeuf@users.noreply.github.com>
2021-04-29 04:43:36 -0700

    Update configfile.go

--- cmd/chirpstack-simulator/cmd/configfile.go
+++ cmd/chirpstack-simulator/cmd/configfile.go
@@ -7,7 +7,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
-	"github.com/brocaar/chirpstack-simulator/internal/config"
+	"github.com/kikeuf/lorawan/internal/config"
 )
 
 const configTemplate = `[general]
