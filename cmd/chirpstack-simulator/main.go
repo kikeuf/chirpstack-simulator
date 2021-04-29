4822cc93ac2e53ecf932ee0ceb30d944c983cf6f
kikeuf <82982743+kikeuf@users.noreply.github.com>
2021-04-29 06:21:01 -0700

    Update main.go

--- cmd/chirpstack-simulator/main.go
+++ cmd/chirpstack-simulator/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/kikeuf/lorawan/cmd/chirpstack-simulator/cmd"
+import "github.com/kikeuf/chirpstack-simulator/cmd/chirpstack-simulator/cmd"
 
 var version string // set by the compiler
 
